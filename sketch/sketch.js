bae2ec174b48a2474d48df92ec95a0024b7e08f7
2501 <kirinokirino@lunariko.com>
2022-05-02 09:53:05 +0300

    Update sketch.js

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -28,7 +28,7 @@ let keys = [];
 const offset = 60-36;
 
 function preload() {
-	json = loadJSON("https://github.com/kirinokirino/p5-piano-midi/blob/master/assets/Ghibli.json");
+	json = loadJSON("/assets/Ghibli.json");
 }
 
 function setup() {
