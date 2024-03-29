2d2c41c518a3c40561e6c710510801e37e88adff
kirinokirino <helloworldagain2@gmail.com>
2024-03-29 18:45:20 +0100

    Add README.md, make it play

---
+++ README.md
@@ -0,0 +1,2 @@
+## A p5 keyboard that plays a midi song (from a json file)
+
--- sketch/sketch.js
+++ sketch/sketch.js
@@ -4,7 +4,7 @@ const RESOLUTION = new p5.Vector(1280, 720);
 let playTime = 0;
 let currentTick = 0;
 
-const VOLUME = 0.2;
+const VOLUME = 0.5;
 
 let osc = new p5.Oscillator("triangle");
 let tempos = [];
@@ -27,7 +27,7 @@ let debugLines = [];
 const octave = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
 const octaves = ["1", "2", "3", "4", "5", "6"];
 let keys = [];
-const offset = 60-36;
+const offset = 60 - 36;
 
 function preload() {
 	json = loadJSON("assets/Ghibli.json");
@@ -35,52 +35,52 @@ function preload() {
 
 let midi = null;  // global MIDIAccess object
 function onMIDISuccess(midiAccess) {
-  console.log("MIDI ready!");
-  midi = midiAccess;  // store in the global (in real usage, would probably keep in an object instance)
+	console.log("MIDI ready!");
+	midi = midiAccess;  // store in the global (in real usage, would probably keep in an object instance)
 
-  handleMIDI(midi);
+	handleMIDI(midi);
 }
 
 function onMIDIMessage(event) {
-  let str = `MIDI message received at timestamp ${event.timeStamp}[${event.data.length} bytes]: `;
-  for (const character of event.data) {
-    str += `0x${character.toString(16)} `;
-  }
-  console.log(str);
-  let message = parseMIDIMessage(event.data);
-  console.log(message);
-  if (message.event == "note-on") {
-  	setPlaying(message.note);
-  } else if (message.event == "note-off") {
-  	noteOff()
-  }
+	let str = `MIDI message received at timestamp ${event.timeStamp}[${event.data.length} bytes]: `;
+	for (const character of event.data) {
+		str += `0x${character.toString(16)} `;
+	}
+	console.log(str);
+	let message = parseMIDIMessage(event.data);
+	console.log(message);
+	if (message.event == "note-on") {
+		setPlaying(message.note);
+	} else if (message.event == "note-off") {
+		noteOff()
+	}
 }
 
 function handleMIDI(midiAccess, indexOfPort) {
-  midiAccess.inputs.forEach((entry) => {entry.onmidimessage = onMIDIMessage;});
+	midiAccess.inputs.forEach((entry) => { entry.onmidimessage = onMIDIMessage; });
 }
 
 function onMIDIFailure(msg) {
-  console.error(`Failed to get MIDI access - ${msg}`);
+	console.error(`Failed to get MIDI access - ${msg}`);
 }
 
 
 function setup() {
 	navigator.requestMIDIAccess().then(onMIDISuccess, onMIDIFailure);
-	
+
 	console.log(MIDIInputMap)
 	createCanvas(RESOLUTION.x, RESOLUTION.y);
-	background(0,0,0, 255);
-	stroke(160,160,160, 255);
-	fill(100,100,100,255);
+	background(0, 0, 0, 255);
+	stroke(160, 160, 160, 255);
+	fill(100, 100, 100, 255);
 	textSize(16);
 	osc.amp(0);
 	osc.freq(261);
-	
+
 	if (getAudioContext().state !== 'running') {
 		osc.start();
 	}
-	
+
 	for (let tempo of json.header.tempos) {
 		tempos.push([tempo.bpm, tempo.ticks]);
 	}
@@ -131,22 +131,22 @@ function mousePressed() {
 
 
 function setPlaying(note) {
-	nowPlaying.push({ "name": keys[note], "preview": true });
+	//nowPlaying.push({ "name": keys[note], "preview": true });
 	lastAction = "Note " + keys[note];
 	osc.freq(midiToFreq(note));
 	osc.amp(VOLUME);
 }
 
 function stahhp() {
-	osc.amp(0);
+	osc.amp(0, 0.5);
 }
 
 function noteOff() {
-	
-  // ramp amplitude to 0 over 0.5 seconds
-  nowPlaying = nowPlaying.filter((note) => { note.preview === false });
-  lastAction = "Note off";
-  osc.amp(0, 0.2);
+
+	// ramp amplitude to 0 over 0.5 seconds
+	nowPlaying = nowPlaying.filter((note) => { note.preview === false });
+	lastAction = "Note off";
+	osc.amp(0, 0.5);
 }
 
 function mouseReleased() {
@@ -155,27 +155,27 @@ function mouseReleased() {
 
 function keyReleased() {
 	lastAction = `Release keycode ${keyCode}`;
-	if (keyCode === 32) {
+	if (keyCode === 49) {
 		lastAction = "Reset time";
 		playTime = 0;
 		currentTick = 0;
 		oldestPlayingNote = { "index": 0, "set": false };
 	}
-	return false; 
+	return false;
 }
 
 function draw() {
-	background(0,0,0, 255);/*
+	background(0, 0, 0, 255);
 	updateTime(deltaTime);
-	*/
-	
+
+
 	if (getAudioContext().state !== 'running') {
 		debugLines.push("AudioContext state is " + getAudioContext().state);
 	}
-	debugLines.push("Press space to restart.");
+	debugLines.push("Press 1 to restart.");
 	debugLines.push(`DELTA: ${Math.floor(deltaTime)} | TIME: ${Math.floor(playTime)} | TICK: ${Math.floor(currentTick)} | BPM: ${Math.floor(currentTempo)}`);
 	debugLines.push(lastAction);
-	
+
 	let notesCount = octave.length * octaves.length;
 	let manuallyPlaying = structuredClone(nowPlaying);
 	oldestPlayingNote.set = false;
@@ -190,7 +190,7 @@ function draw() {
 			nowPlaying.push(notes[i]);
 		} else if (currentTick >= noteEnd && oldestPlayingNote.set) break;
 	}
-	
+
 	let keySize = RESOLUTION.x / keys.length;
 	let playingNotes = nowPlaying.map((note) => { return note.name });
 	for (let i = 0; i < keys.length; i++) {
@@ -198,67 +198,67 @@ function draw() {
 		let y = RESOLUTION.y / 2;
 		let label = keys[i];
 		let brightness = keys[i].includes("#") ? 0.3 : 1;
-    let keyHeight = y
+		let keyHeight = y
 		if (playingNotes.includes(keys[i])) {
-      brightness *= 2;
-      keyHeight *= .9;
-    }
+			brightness *= 2;
+			keyHeight *= .9;
+		}
 		stroke(150);
-		fill(brightness * 100,brightness * i * 2,brightness * i * 3);
+		fill(brightness * 100, brightness * i * 2, brightness * i * 3);
 		rect(x, y, keySize, keyHeight);
 		fill(255);
 		stroke(0);
 		push();
 		translate(x, y);
-		rotate(PI/2);
+		rotate(PI / 2);
 		text(label, 0, 0);
 		pop();
 	}
 	if (nowPlaying[0] == undefined) {
 		stahhp()
 	} else {
-		
-	
-    for (let note of nowPlaying) {
-    	if (debugLines[2]) {
-    		debugLines[2] += ", " + note.name;
-    	}	else {
-    		debugLines.push(note.name);
-    		setPlaying(note.midi);
-    	}
-    }}
+		for (let note of nowPlaying) {
+			if (debugLines[2]) {
+				debugLines[2] += ", " + note.name;
+			} else {
+				debugLines.push(note.name);
+			}
+			console.log(note.midi)
+			setPlaying(note.midi);
+		}
+	}
 	for (let i = 0; i < debugLines.length; i++) {
 		text(debugLines[i], 10, 20 + 20 * i);
 	}
 	nowPlaying = manuallyPlaying;
-    debugLines = [];
+	debugLines = [];
 }
 
 /*
 https://github.com/hhromic/midi-utils-js/blob/master/midiparser.js
 */
 function parseMIDIMessage(bytes) {
-        if (bytes instanceof Uint8Array && bytes.length > 0) {
-            var status = bytes[0] & 0xF0;
-            var channel = bytes[0] & 0x0F;
-            switch (status) {
-                case 0x80: // Note-Off event
-                    if (bytes.length > 1) {
-                        var note = bytes[1] & 0x7F;
-                        var velocity = bytes[2] & 0x7F;
-                        return { event: "note-off", channel, note, velocity};
-                    }
-                    return;
-                case 0x90: // Note-On event
-                    if (bytes.length > 2) {
-                        var note = bytes[1] & 0x7F;
-                        var velocity = bytes[2] & 0x7F;
-                        if (velocity > 0x00)
-                        return { event: "note-on",channel,note, velocity};
-                        else
-                        return { event: "note-off", channel, note, velocity};
-                    }
-                    return;
-              }
-              }
+	if (bytes instanceof Uint8Array && bytes.length > 0) {
+		var status = bytes[0] & 0xF0;
+		var channel = bytes[0] & 0x0F;
+		switch (status) {
+			case 0x80: // Note-Off event
+				if (bytes.length > 1) {
+					var note = bytes[1] & 0x7F;
+					var velocity = bytes[2] & 0x7F;
+					return { event: "note-off", channel, note, velocity };
+				}
+				return;
+			case 0x90: // Note-On event
+				if (bytes.length > 2) {
+					var note = bytes[1] & 0x7F;
+					var velocity = bytes[2] & 0x7F;
+					if (velocity > 0x00)
+						return { event: "note-on", channel, note, velocity };
+					else
+						return { event: "note-off", channel, note, velocity };
+				}
+				return;
+		}
+	}
 }
